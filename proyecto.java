422c2890c18808afc063dbfe472346518ba6cca5
ramosjr18 <106755836+ramosjr18@users.noreply.github.com>
2022-06-14 22:38:40 +0200

    Update proyecto.java

--- proyecto.java
+++ proyecto.java
@@ -3,6 +3,6 @@ class ola{
     Scanner input = new Scanner(System.in);
     double a = input.nextDouble();
     System.out.println(a);
-    System.out.println("Puto el que lo lea");
+    System.out.println("Puto");
     
 }
