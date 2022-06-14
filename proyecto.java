8e2f86ba83619c061b4cbf7c0b0ec372063679ec
ramosjr18 <106755836+ramosjr18@users.noreply.github.com>
2022-06-14 22:39:11 +0200

    Update proyecto.java

--- proyecto.java
+++ proyecto.java
@@ -2,7 +2,9 @@ import java.util.*;
 class ola{
     Scanner input = new Scanner(System.in);
     double a = input.nextDouble();
+    int b;
     System.out.println(a);
     System.out.println("Puto");
+    
     
 }
